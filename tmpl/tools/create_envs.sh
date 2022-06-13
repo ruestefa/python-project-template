f1de09068b0351522f0bdf4395f2e2c7683d0b14
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-06-13 12:19:45 +0200

    implement option to recreate envs w/o updating them

--- tmpl/tools/create_envs.sh
+++ tmpl/tools/create_envs.sh
@@ -28,6 +28,13 @@ if [ ${#env_names[@]} -eq 0 ]; then
     env_names=("${default_env_names[@]}")
 fi
 
+UPDATE=${UPDATE:-true}
+if ${UPDATE}; then
+    echo "update environments from requirements"
+else
+    echo "recreate environments"
+fi
+
 detect_conda()
 {
     mamba --version 1>/dev/null 2>&1
@@ -40,7 +47,7 @@ detect_conda()
         echo conda
         return 0
     fi
-    echo "error: neither mamba nor conda detectec" >&2
+    echo "error: neither mamba nor conda detected" >&2
     return 1
 }
 
@@ -49,44 +56,64 @@ cmd=(${CONDA} --version)
 echo "\$ ${cmd[@]^Q}"
 eval "${cmd[@]}" || exit
 
-create_env()
+remove_existing_env()
+{
+    local env_name="${1}"
+    if $(eval ${CONDA} info --env | \grep -q "^\<${env_name}\>"); then
+        echo "remove conda env '${env_name}'"
+        local cmd=(${CONDA} env remove -n "${env_name}")
+        echo "\$ ${cmd[@]^Q}"
+        eval "${cmd[@]}" || return 1
+    fi
+}
+
+create_updated_env()
 {
     local env_name="${1}"
     local env_file="${2}"
     shift 2
     local reqs_files=("${@}")
+    echo "create up-to-date conda env '${env_name}' from ${reqs_files[@]}"
     local reqs_file_flags=()
     local reqs_file
-    if $(eval ${CONDA} info --env | \grep -q "^\<${env_name}\>"); then
-        echo "remove conda env '${env_name}'"
-        cmd=(${CONDA} env remove -n "${env_name}")
-        echo "\$ ${cmd[@]^Q}"
-        eval "${cmd[@]}" || return 1
-    fi
-
-    echo "create conda env '${env_name}' from ${reqs_files[@]}"
     for reqs_file in "${reqs_files[@]}"; do
         reqs_file_flags+=(--file="${reqs_file}")
     done
-    cmd=(${CONDA} create -n "${env_name}" python==${PYTHON_VERSION} "${reqs_file_flags[@]}" --yes )
+    local cmd=(${CONDA} create --yes -n "${env_name}" python==${PYTHON_VERSION} "${reqs_file_flags[@]}")
     echo "\$ ${cmd[@]^Q}"
     eval "${cmd[@]}" || return 1
-
     echo "export conda env '${env_name}' to ${env_file}"
-    cmd=(${CONDA} env export -n "${env_name}" --no-builds)
+    local cmd=(${CONDA} env export -n "${env_name}" --no-builds)
     echo "\$ ${cmd[@]^Q}"
     eval "${cmd[@]}" > "${env_file}" || return 1
+    return 0
+}
 
+recreate_env()
+{
+    local env_name="${1}"
+    local env_file="${2}"
+    echo "recreate conda env '${env_name}' from ${env_file}"
+    local cmd=(${CONDA} env create -n "${env_name}" python==${PYTHON_VERSION} --file="${env_file}")
+    echo "\$ ${cmd[@]^Q}"
+    eval "${cmd[@]}" || return 1
     return 0
 }
 
 for env_name in "${env_names[@]}"; do
-    case "${env_name}" in
-        "${run_env_name}")
-            create_env "${run_env_name}" "${run_env_file}" "${run_reqs_file}" || exit
+    remove_existing_env "${env_name}"
+    case "${env_name}/${UPDATE}" in
+        "${run_env_name}/false")
+            recreate_env "${run_env_name}" "${run_env_file}" || exit
         ;;
-        "${dev_env_name}")
-            create_env "${dev_env_name}" "${dev_env_file}" "${run_reqs_file}" "${dev_reqs_file}" || exit
+        "${dev_env_name}/false")
+            recreate_env "${dev_env_name}" "${dev_env_file}" || exit
+        ;;
+        "${run_env_name}/true")
+            create_updated_env "${run_env_name}" "${run_env_file}" "${run_reqs_file}" || exit
+        ;;
+        "${dev_env_name}/true")
+            create_updated_env "${dev_env_name}" "${dev_env_file}" "${run_reqs_file}" "${dev_reqs_file}" || exit
         ;;
         *)
             echo "error: invalid env_name '${env_name}'; choices: ${default_env_names[@]}" >&2
